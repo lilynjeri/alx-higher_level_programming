c524caa21f2f0705627cb489fbb6697c62494681
lilynjeri <liliann.mwangi@gmail.com>
2024-05-08 08:56:14 -0700

    Task 1 ; 1-calculation.py commit

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Imports functions from calculator_1.py and performs Maths operations."""
+    a = 10
+    b = 5
+    from calculator_1 import add, sub, mul, div
+    result_add = add(a, b)
+    result_sub = sub(a, b)
+    result_mul = mul(a, b)
+    result_div = div(a, b)
+    print(f"{a} + {b} = {result_add}")
+    print(f"{a} - {b} = {result_sub}")
+    print(f"{a} * {b} = {result_mul}")
+    print(f"{a} / {b} = {result_div}")
